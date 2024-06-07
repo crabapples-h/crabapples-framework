a5a607ca47f6d67307ea8a8acfe281a99e22b4fb
一朵小蔷薇 <crabapples.cn@gmail.com>
2024-06-07 17:17:01 +0800

    bak1

--- code-generator/src/main/java/cn/crabapples/generator/CrabapplesControllerProcessor.java
+++ code-generator/src/main/java/cn/crabapples/generator/CrabapplesControllerProcessor.java
@@ -1,6 +1,7 @@
 package cn.crabapples.generator;
 
 import com.google.auto.service.AutoService;
+import com.sun.source.util.Trees;
 import net.bytebuddy.ByteBuddy;
 import net.bytebuddy.description.modifier.Visibility;
 import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
@@ -28,6 +29,7 @@ public class CrabapplesControllerProcessor extends AbstractProcessor {
 
     private Filer filer;
     private Messager messager;
+    private ClassLoader classLoader;
 
     //支持的java版本
     @Override
@@ -38,7 +40,14 @@ public class CrabapplesControllerProcessor extends AbstractProcessor {
     public synchronized void init(ProcessingEnvironment processingEnv) {
         this.filer = processingEnv.getFiler();
         this.messager = processingEnv.getMessager();
+        this.classLoader = this.getClass().getClassLoader();
         super.init(processingEnv);
+        Trees trees = Trees.instance(processingEnv);
+        if (trees instanceof JavacTrees) {
+            javacTrees = (JavacTrees) trees;
+        } else {
+            throw new IllegalStateException("Unexpected Trees implementation");
+        }
 
     }
 
@@ -54,31 +63,39 @@ public class CrabapplesControllerProcessor extends AbstractProcessor {
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
         messager.printMessage(Diagnostic.Kind.MANDATORY_WARNING, "执行process()方法");
         System.err.println("start EnableTestAnno.process()");
-        for (Element rootElement : roundEnv.getRootElements()) {
-            System.err.println(rootElement);
-        }
+//        for (Element rootElement : roundEnv.getRootElements()) {
+//            System.err.println(rootElement);
+//        }
 
         Set<? extends Element> elementsAnnotatedWith = roundEnv.getElementsAnnotatedWith(CrabapplesController.class);
         for (Element element : elementsAnnotatedWith) {
             try {
                 TypeElement typeElement = ((TypeElement) element);
                 String className = typeElement.getQualifiedName().toString();
-                System.err.println(className);
-                createByByteBuddy(className);
+//                System.err.println(className);
+//                createByByteBuddy(className);
+                createByByteBuddy(typeElement);
                 System.err.println("---------------------------end");
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        return false;
+        return true;
     }
 
 
-    private void createByByteBuddy(String className) throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-        String newFileName = className + "Controller";
-        System.err.println(newFileName);
+    private void createByByteBuddy(TypeElement typeElement) throws IOException, ClassNotFoundException {
+        String className = typeElement.getQualifiedName().toString();
+//        String newFileName = className + "AAAAA";
+        Trees trees = Trees.instance(processingEnv);
+        System.err.println(className);
+//        Class<?> aClass = classLoader.loadClass("target/classes/"+className);
+//        System.err.println(aClass);
+//        messager.printMessage(Diagnostic.Kind.MANDATORY_WARNING, aClass.getName());
+
         new ByteBuddy()
-                .rebase(Object.class)
+//                .subclass(typeElement)
+                .subclass(Class.forName(className))
 //                .rebase(BaseController.class)
 //                .subclass(BaseController.class)
 //                .rebase(Object.class)
@@ -87,7 +104,34 @@ public class CrabapplesControllerProcessor extends AbstractProcessor {
 //                .subclass(BaseController.class)
 //                .rebase(ControllerTemplate.class)
 //                .rebase(Class.forName(newFileName))
-                .name(newFileName)
+//                .name(newFileName)
+                // 添加属性
+                .defineField("field2", String.class, Visibility.PRIVATE)
+
+                .make()
+                .load(ClassLoader.getSystemClassLoader(), ClassLoadingStrategy.Default.CHILD_FIRST)
+                .saveIn(new File("src/main/java/"));
+//                .saveIn(new File("target/classes/"));
+    }
+
+    private void createByByteBuddy(String className) throws IOException, ClassNotFoundException {
+//        String newFileName = className + "AAAAA";
+        System.err.println(className);
+//        Class<?> aClass = classLoader.loadClass("target/classes/"+className);
+//        System.err.println(aClass);
+//        messager.printMessage(Diagnostic.Kind.MANDATORY_WARNING, aClass.getName());
+
+        new ByteBuddy()
+                .subclass(Class.forName(className))
+//                .rebase(BaseController.class)
+//                .subclass(BaseController.class)
+//                .rebase(Object.class)
+//                .subclass(Object.class)
+//                .redefine(Object.class)
+//                .subclass(BaseController.class)
+//                .rebase(ControllerTemplate.class)
+//                .rebase(Class.forName(newFileName))
+//                .name(newFileName)
                 // 添加属性
                 .defineField("field2", String.class, Visibility.PRIVATE)
 
